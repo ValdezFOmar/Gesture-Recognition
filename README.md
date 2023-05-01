4f03ca8164f20cbb9a043a07e0ec39a8224f37ca
Omar <omarantoniovaldef2@gmail.com>
2023-04-30 23:24:28 -0700

    Set up Dataset. TODO: Train model

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gesture Recognition
+Simple model for gesture recognition using mediapipe packages.
+
+Note: Rename 'nothing' directory and image to 'none'.
+
+## Packages
+- requirements.txt
+
+## Dataset
+- [ASL Sign language](https://www.kaggle.com/datasets/grassknoted/asl-alphabet?resource=download)
+- [Rock Paper Scissors](https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/rps_data_sample.zip)
--- src/asl_recognition.py
+++ src/asl_recognition.py
@@ -1,4 +1,3 @@
-# from google.colab import files
 import os
 import tensorflow as tf
 assert tf.__version__.startswith('2')
@@ -7,7 +6,8 @@ from mediapipe_model_maker import gesture_recognizer
 
 import matplotlib.pyplot as plt
 
-DATASET_PATH = "rps_data_sample"
+# ASL Sign Language Gestures
+DATASET_PATH = "asl_alphabet_train"
 
 # Print labels
 print(DATASET_PATH)
@@ -15,22 +15,8 @@ labels = []
 for i in os.listdir(DATASET_PATH):
   if os.path.isdir(os.path.join(DATASET_PATH, i)):
     labels.append(i)
+labels.sort()
 print(labels)
-
-# example images for each gesture
-# NUM_EXAMPLES = 5
-
-# for label in labels:
-#   label_dir = os.path.join(dataset_path, label)
-#   example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
-#   fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10,2))
-#   for i in range(NUM_EXAMPLES):
-#     axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
-#     axs[i].get_xaxis().set_visible(False)
-#     axs[i].get_yaxis().set_visible(False)
-#   fig.suptitle(f'Showing {NUM_EXAMPLES} examples for {label}')
-
-# plt.show()
 
 # Load he dataset
 data = gesture_recognizer.Dataset.from_folder(
