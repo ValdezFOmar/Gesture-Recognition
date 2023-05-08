abd31694021d4c3d912c7753955f5a666b4d6624
Omar <omarantoniovaldef2@gmail.com>
2023-05-07 23:06:04 -0700

    Better prediction display

--- tests/asl_live_test.py
+++ tests/asl_live_test.py
@@ -9,21 +9,27 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 MODEL_PATH = 'models/asl_model/gesture_recognizer.task'
 
-INVALID_OUTPUTS = ['none','space','del']
-
+default_value = ('none',0)
+prediction = default_value
 temp = ''
 
 # This callback is called whenever the task has finished processing a video frame
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
-    if not(len(result.gestures) > 0): return
-    category_name = result.gestures[0][0].category_name
-    
+    global prediction
     global temp
-    if category_name.lower() in INVALID_OUTPUTS or category_name == temp: return
-    temp = category_name
     
+    if not(len(result.gestures) > 0):
+        prediction = default_value
+        return
+    
+    category_name = result.gestures[0][0].category_name
     score = result.gestures[0][0].score
-    print(f'Result: {category_name}         Score: {score:.2f}\n')
+    
+    # if category_name == temp: return
+    # temp = category_name
+    
+    # print(f'Result: {category_name}   Score: {score:.2f}\n')
+    prediction = (category_name, score)
 
 # Create a gesture recognizer instance with the live stream mode:
 options = GestureRecognizerOptions(
@@ -43,8 +49,20 @@ with GestureRecognizer.create_from_options(options) as RECOGNIZER:
         frame_timestamp_ms = int(cap.get(cv2.CAP_PROP_POS_MSEC)) # The recognzer only accepts type int
         RECOGNIZER.recognize_async(mp_image, frame_timestamp_ms)
         
+        text = f'Sign: {prediction[0]}      Score: {prediction[1]:.2f}'
+        
+        fliped_frame = cv2.flip(frame, 1)
+        
         # Show to screen
-        cv2.imshow('Gesture-Recognizer: American Sign language (ASL)', cv2.flip(frame, 1))
+        cv2.putText(img=fliped_frame,
+                    text=text,
+                    org=(100,50),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=1,
+                    color=(0,0,255),
+                    thickness=3)
+        
+        cv2.imshow('Gesture-Recognizer: American Sign language (ASL)', fliped_frame)
         
         # Break and close camera feed
         if cv2.waitKey(10) & 0xFF == ord('q'):
