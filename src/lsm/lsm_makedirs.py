0f35e6aedbd105ffa9b14c7f6a40208898c88a05
Omar <omarantoniovaldezf2@gmail.com>
2023-05-22 23:10:11 -0700

    Reorganized folders and add script for lsm

---
+++ src/lsm/lsm_makedirs.py
@@ -0,0 +1,47 @@
+import os
+
+
+LABELS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "ñ",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "amor",
+    "hola",
+    "yo",
+    "none",
+    "espacio",
+]
+DATA_PATH = "lsm_train"
+
+
+def main():
+    for label in LABELS:
+        label_directory = os.path.join(DATA_PATH, label)
+        os.makedirs(label_directory, exist_ok=True)
+
+
+if __name__ == "__main__":
+    main()
